58ade44018fbb5251f43bd8ee23f6a73052204e1
Thomas Clausen <tntc1978@gmail.com>
2015-04-01 10:25:25 +0200

    Update archive-product.php
    By changing 
    '<?php do_action( 'woocommerce_archive_description' ); ?>' 
    to 
    '<?php echo term_description( '', get_query_var( 'taxonomy' ) ); ?>'
    we would get support for custom post taxonomies.
    
    The archive page will still display the description of the regular tags and categories, but you will add support for the many custom taxonomies that people create. And support the "native" WordPress function.

--- templates/archive-product.php
+++ templates/archive-product.php
@@ -15,15 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header( 'shop' ); ?>
 
-	<?php
-		/**
-		 * woocommerce_before_main_content hook
-		 *
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 */
-		do_action( 'woocommerce_before_main_content' );
-	?>
+<?php echo term_description( '', get_query_var( 'taxonomy' ) ); ?>
 
 		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 
